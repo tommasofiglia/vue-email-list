a8bbf3ed7e7c8fc44a08300fd8c9d016454203c7
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-10 23:40:24 +0100

    Bonus: al click si genera il numero random

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -6,4 +6,25 @@
 let app = new Vue({
   // options object
   el: "#app",
+  data:{
+    randomNumber: "",
+    position: "",
+  },
+  methods: {
+    //Funzione che, al click sul quadrato, assegna il valore numerico relativo alla posizione del quadrato all'oggetto position.
+
+    numberOnClick(index) {
+      this.position = index;
+      console.log(this.position);
+
+    //Utilizzo axios per trovare il numero random tra 1 e 9.
+      axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+      .then(number => {
+      this.randomNumber = (number.data.response);
+      console.log(this.randomNumber);
+      alert('Il numero generato è ' +  this.randomNumber);
+      });
+
+    }
+  }
 });
