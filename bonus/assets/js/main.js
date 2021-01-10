c76dd3d6347fba90759c5e811021356b5aec20a8
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-10 22:40:46 +0100

    Bonus: HTML e CSS configurati

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,9 @@
+// Generate una Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato
+
+
+let app = new Vue({
+  // options object
+  el: "#app",
+});
